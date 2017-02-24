072cb00adfb99f5bae3a4c66ccde73d056b2b82e
Calvin Metcalf <cmetcalf@appgeo.com>
2017-02-24 15:00:55 -0500

    remove comments

--- index.js
+++ index.js
@@ -1,12 +1,4 @@
-// var PouchSync = require('pouch-websocket-sync');
-// var Websocket = require('websocket-stream');
-// var psms = require('pouch-stream-multi-sync');
+
 var Transform = require('readable-stream/transform');
-// var inherits = require('inherits')
-// var WebSocketServer = require('ws').Server
-// var through = require('through2')
-// var duplexify = require('duplexify')
-// var WS = require('ws')
 var TransformStream = require('stream').Transform;
 root.innerText = `should be function: ${typeof TransformStream}`
-// var prs = require('pouch-remote-stream');
